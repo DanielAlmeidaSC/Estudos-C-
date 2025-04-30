1492ec4226b41fa10f0f7de9e93a80340298cabf
DanielAlmeidaSC <eexpeditodaniel@gmail.com>
2025-04-30 13:47:13 -0300

    new codes

---
+++ var/programa003/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Programa003 {
+    internal class Program {
+        static void Main(string[] args) {
+            // Use `var` para armazenar o resultado de uma chamada de função que retorna uma `string`.
+
+        string ObterResultado(){
+            return "Olá rapeize";
+        }
+            var mensagem = ObterResultado();
+            Console.WriteLine(mensagem);
+        }
+    }
+}
---
+++ var/programa004/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Programa004 {
+    internal class Program {
+        static void Main(string[] args) {
+            //Crie uma lista de `int` usando `var` e adicione alguns números.
+
+            var lista = new List<int>(); //cria um list. var sabe que é um list por conta do valor declarado
+
+            lista.Add(10);
+            lista.Add(10);
+            lista.Add(10);
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]+10);
+            }
+
+            
+        }
+    }
+}
